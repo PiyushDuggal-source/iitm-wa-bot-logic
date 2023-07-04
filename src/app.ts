030652cba57fda3b653d3fcfff59aa4b8bae4cea
Piyush Duggal <duggalpiyush0@gmail.com>
2023-07-05 01:00:01 +0530

    added middleware in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import e from 'express'
 import router from './routes'
+import { checkAPI } from './middleware'
 
 const app = e()
 
+app.use(e.json())
 app.use(e.urlencoded({ extended: true }))
+app.use(checkAPI)
 app.use(router)
 
 
