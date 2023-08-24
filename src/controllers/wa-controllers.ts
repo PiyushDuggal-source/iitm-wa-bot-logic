5157e87a8eecf313c3ae3653bc920fd6ef26b75b
Piyush Duggal <duggalpiyush0@gmail.com>
2023-08-25 03:48:39 +0530

    modified wa controller

--- src/controllers/wa-controllers.ts
+++ src/controllers/wa-controllers.ts
@@ -1,36 +1,37 @@
 import { sendMessageToBot, sendMessageToUser } from "../services/whatsApp";
-import { Req_type, ReturnType } from "../types";
+import { Req_type } from "../types";
 import { createCmdResponse } from "./cmd-controller";
 import { getUserData } from "./user-controllers";
 
-export const processMessageSend = async (messageObject: Req_type) => {
+export const processMessageSend = async (
+  messageObject: Req_type,
+  commandRes: string,
+) => {
   console.log("\nEntering processMessageSend");
-  console.log(messageObject);
-  const { name, chatId, messageBody: cmd } = messageObject;
+  const { name, chatId, cmd } = messageObject;
   const userInfo = await getUserData({
     name,
     chatId,
   });
 
-  const commandRes = await createCmdResponse(cmd);
-  if (commandRes === "No Notes Found") {
-    return {
-      status: "No such command",
-    };
-  }
   if (userInfo.newUser) {
     console.log("New user");
     const welcomeMessage = `Hi ${name}, Welcome to the WhatsApp Bot! This is your first time using the bot!\nGood Luck!!`;
   }
 
   const finalMsg = `\n${commandRes}\n`;
+  console.log(userInfo)
 
-  // await sendMessageToUser({
-  //   message: finalMsg,
-  //   chatId,
-  //   type: "SUCCESS",
-  //   emoji: "✅",
-  // });
+  if (userInfo.role === "OWNER") {
+    return sendMessageToBot({
+      message: finalMsg,
+    });
+  }
+
+  await sendMessageToUser({
+    message: finalMsg,
+    chatId,
+  });
 
   // TODO: Add logic to check
   console.log("Leaving processMessageSend\n");
