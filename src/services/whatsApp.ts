b7874e4699686b8f3beb9aa46c6469c52aa28b78
Piyush Duggal <duggalpiyush0@gmail.com>
2023-08-28 00:09:04 +0530

    log fix

--- src/services/whatsApp.ts
+++ src/services/whatsApp.ts
@@ -2,12 +2,12 @@ import axiosInstance from "../axios";
 import { Res_Type } from "../types";
 
 export const sendMessageToBot = ({ message }: { message: string }) => {
-  console.log("\nEntering sendMessageToBot" + "\n" + "message: " + message);
+  console.log("\nEntering sendMessageToBot");
   return axiosInstance.post("/wa-service/api/sendMsgToBot", { message });
 };
 
 export const sendMessageToUser = ({ message, chatId }: Res_Type) => {
-  console.log("\nEntering sendMessageToUser" + "\n" + "message: " + message);
+  console.log("\nEntering sendMessageToUser");
   return axiosInstance.post("/wa-service/api/sendMessage", {
     message,
     chatId,
