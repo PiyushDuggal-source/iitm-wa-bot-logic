ccbe070aa324f455f06415787737eba1cef9d7d7
Piyush Duggal <duggalpiyush0@gmail.com>
2024-02-07 17:47:46 +0530

    added quotes of the day

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import * as dotenv from "dotenv";
 import { connectToDb } from "./utils/db";
+import './utils/cron'
 dotenv.config();
 
 const port = process.env.PORT || 3001;
@@ -9,3 +10,4 @@ connectToDb(process.env.DB_URL as string);
 app.listen(port, () => {
   console.log(`IITM-WA-BOT-Logic app is listening on port ${port}`);
 });
+
---
+++ src/utils/cron.ts
@@ -0,0 +1,13 @@
+import cron from "node-cron";
+import { getQuotes } from "../actions/cronjobs";
+import { sendMessageToBot } from "../services/whatsApp";
+
+// run every day at 12 PM
+
+cron.schedule("0 12 * * *", async () => {
+  const data = await getQuotes();
+
+  const message = `*Quote of the day!* 💭\n\nCategory: *${data.category}*\n\n_*"${data.text}"*_\n                                                                                                     - ${data.author}`;
+
+  sendMessageToBot({ message, groupId: process.env.GROUP_ID as string });
+});
